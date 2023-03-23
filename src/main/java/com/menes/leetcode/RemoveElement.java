c0d7f3fb4822a4b7d30839c7cf2e3c76d19d5efb
Duong Duy Men <20130072@st.hcmuaf.edu.vn>
2023-03-23 09:50:28 +0700

    BEATS 100% Solution #27

--- src/main/java/com/menes/leetcode/RemoveElement.java
+++ src/main/java/com/menes/leetcode/RemoveElement.java
@@ -1,4 +1,18 @@
 package com.menes.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums){
+            if(i != val) list.add(i);
+        }
+        for(int i = 0;i<list.size();i++){
+            nums[i] = list.get(i);
+        }
+        return list.size();
+
+    }
 }
