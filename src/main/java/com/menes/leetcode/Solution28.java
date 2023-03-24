e454aabfe5562adb66e4304a68915adb1201b48c
Duong Duy Men <20130072@st.hcmuaf.edu.vn>
2023-03-24 14:52:51 +0700

    Solution #28

---
+++ src/main/java/com/menes/leetcode/Solution28.java
@@ -0,0 +1,11 @@
+package com.menes.leetcode;
+
+public class Solution28 {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0;i<haystack.length();i++){
+            String s = haystack.substring(i,haystack.length());
+            if(s.startsWith(needle)) return i;
+        }
+        return -1;
+    }
+}
