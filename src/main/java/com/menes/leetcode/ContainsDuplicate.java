0f70877d5c23f0c9c276e18a91846fa94135cf47
Duong Duy Men <20130072@st.hcmuaf.edu.vn>
2023-05-09 06:27:43 +0700

    Solution #217

---
+++ src/main/java/com/menes/leetcode/ContainsDuplicate.java
@@ -0,0 +1,12 @@
+package com.menes.leetcode;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0 ;i<nums.length -1;i++)
+            if(nums[i] == nums[i+1]) return true;
+        return false;
+    }
+}
