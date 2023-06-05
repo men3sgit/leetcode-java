6d9055c729452f7fe518d7aed88149448f110e38
Duong Duy Men <menesforenglish102@gmail.com>
2023-06-05 12:19:00 +0700

    BEATS 100% Solution #237

---
+++ src/main/java/com/menes/leetcode/DeleteNodeInALinkedList.java
@@ -0,0 +1,8 @@
+package com.menes.leetcode;
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val=(node.next).val;
+        node.next=(node.next).next;
+    }
+}
