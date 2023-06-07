8dd2ee403346da93c15140817bd19aa5694c8dd5
Duong Duy Men <menesforenglish102@gmail.com>
2023-06-07 21:24:30 +0700

    Solution #240

---
+++ src/main/java/com/menes/leetcode/SearchA2DMatrixII.java
@@ -0,0 +1,15 @@
+package com.menes.leetcode;
+
+public class SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] ints : matrix) {
+            if (ints[ints.length - 1] >= target) {
+                for (int anInt : ints) {
+                    if (anInt == target) return true;
+                }
+
+            }
+        }
+        return false;
+    }
+}
