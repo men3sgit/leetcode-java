0997e7143981112cc1f3517d0ecd5b3ed270f825
Duong Duy Men <menesforenglish102@gmail.com>
2023-06-19 22:46:27 +0700

    Solution #315

---
+++ src/main/java/com/menes/leetcode/CountOfSmallNumbersAfterSelf.java
@@ -0,0 +1,33 @@
+package com.menes.leetcode;
+import java.util.*;
+public class CountOfSmallNumbersAfterSelf {
+    public List<Integer> countSmaller(int[] nums) {
+        int min = 20001;
+        int max = -1;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        min--;
+        int[] count = new int[max-min+1];
+        Integer[] result = new Integer[nums.length];
+        for (int i = nums.length-1; i >=0; i--) {
+            int k = nums[i]-min-1;
+            int c = 0;
+            do {
+                c += count[k];
+                k -= (-k&k);
+            } while (k > 0);
+            result[i] = c;
+
+            k = nums[i]-min;
+            while (k < count.length) {
+                count[k]++;
+                k += (-k&k);
+            }
+        }
+
+        return Arrays.asList(result);
+    }
+}
