56f8cfa399bbeac8a27bbde0cbe2134989dd721c
Duong Duy Men <menesforenglish102@gmail.com>
2023-06-22 21:06:46 +0700

    BEATS 100% Solution #326

---
+++ src/main/java/com/menes/leetcode/PowerOfThree.java
@@ -0,0 +1,11 @@
+package com.menes.leetcode;
+
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if (n == 3 || n == 1) return true;
+        if (n < 3) return false;
+        if (n % 3 == 0)
+            return isPowerOfThree(n / 3);
+        return false;
+    }
+}
